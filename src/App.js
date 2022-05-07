1e9206071e215e11d800860020c169fde8b33e4f
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-05-07 10:48:38 +0530

    useref ex2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import useRefExample1 from './components/useRefExample1'
-import {useRef} from 'react'
+// import useRefExample1 from './components/useRefExample1'
+import React from 'react'
+import UseRefExample2 from './components/UseRefExample2'
+import {useState, useEffect, useRef} from 'react'
 
 function App() {
-const inputRef = useRef();
-const paraRef = useRef();
-const onSubmit = e => {
-  e.preventDefault();
-  console.log(inputRef.current.value);
-  inputRef.current.value = 'Hello';
-  inputRef.current.style.backgroundColor = 'purple';
-  paraRef.current.innerText = 'Goodbye';
-}
+
 
   return (
     <div className='container mt-5'>
-      <form onSubmit={onSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input 
-        type='text' 
-        ref = {inputRef}
-        id='name' 
-        className='form-control mb-2'/>
-        <button type='submit' className='btn btn-primary'>Submit</button>
-        <p
-        onClick={() => inputRef.current.focus()}
-        ref={paraRef}>Hello!</p>
-      </form>
+      <UseRefExample2/>
     </div>
-  )
+    )
 }
 
 export default App
