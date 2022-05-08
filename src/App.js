87855c310f11e3887580bf13b5b5e4206cc8b2ea
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-05-08 12:47:18 +0530

    memory leak error fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // import useRefExample1 from './components/useRefExample1'
 import React from 'react'
-import UseRefExample2 from './components/UseRefExample2'
-import {useState, useEffect, useRef} from 'react'
+// import UseRefExample2 from './components/UseRefExample2'
+// import {useRef} from 'react'
+import UseRefExample3 from './components/UseRefExample3'
 
 function App() {
 
 
   return (
     <div className='container mt-5'>
-      <UseRefExample2/>
+      <UseRefExample3/>
     </div>
     )
 }
