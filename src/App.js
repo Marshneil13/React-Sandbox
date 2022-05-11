dea96d3e9a2c347e36f20ebe835490a815b2be01
marshnil_13 <btech10225.20@bitmesra.ac.in>
2022-05-11 17:49:28 +0530

    Custom hook 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react'
 // import UseRefExample2 from './components/UseRefExample2'
 // import {useRef} from 'react'
 // import UseMemoExample from './components/UseMemoExample'
-import CustomHookExample1 from './components/CustomHookExample1'
+import CustomHookExample2 from './components/CustomHookExample2'
 
 function App() {
  
 
   return (
     <div className='container mt-5'>
-      <CustomHookExample1/>
+      <CustomHookExample2/>
     </div>
     )
 }
